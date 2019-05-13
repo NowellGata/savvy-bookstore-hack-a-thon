b2c49070eaa76549cdb16766ebb15020d4aa9e63
manavm1990 <manavm@visionwebsoft.com>
2019-05-13 17:01:35 -0500

    Add Header state with title.

---
+++ components/Header.js
@@ -0,0 +1,3 @@
+export default function (headState) {
+  return `<h1>${headState.title}</h1>`
+}
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+import Header from "./components/Header";
 import Navigation from "./components/Navigation";
 
 const root = document.querySelector('#root');
 
 const states = {
+    header: {
+        title: "Welcome to Savvy BookStore"
+    },
     nav: {
         links: ['Books', 'Albums']
     }
@@ -10,6 +14,7 @@ const states = {
 
 function render(state){
     root.innerHTML =`
+    ${Header(state.header)}
     ${Navigation(state.nav)}
     `;
 }
