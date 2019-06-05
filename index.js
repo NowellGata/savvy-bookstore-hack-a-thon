21df0caa50c310f1f810fbb4d37d67f3bf908534
Nowell Gata <nowell.gata@gmail.com>
2019-06-04 19:08:55 -0500

    Refactor States to use products with an array of books and albums

--- index.js
+++ index.js
@@ -12,8 +12,10 @@ const states = {
     'navigation': {
         'links': [ 'books', 'albums' ],
     },
+    'products':[
+      {
     'books': [
-        {
+
             'id': 1,
             'title': 'Lasagna: A Retrospective',
             'creator': 'Garfield',
@@ -25,8 +27,10 @@ const states = {
                 'The essential guide to Italian casseroles of all types.',
                 "Real G's move silent, like Lasagna. -Lil Wayne",
             ],
-        },
+      'albums': [] ,
+          },
     ],
+  ],
 };
 
 function render(state){
